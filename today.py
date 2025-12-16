1b7c46a953b2e48280bc650948b9a095b22f98b7
halimcan <albayhalim@gmail.com>
2025-12-16 18:06:06 +0300

    new buddy function added

--- today.py
+++ today.py
@@ -6,3 +6,9 @@ This is the module docstring
 pass  # YOUR CODE HERE
 def my_name_is():
     return "halimcan"
+
+"""A module computing buddy pair names for the day"""
+
+def my_buddy_is():
+    """Return my buddy's GitHub username."""
+    return "halimcan"
